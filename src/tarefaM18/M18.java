00a8ecad66096e4c6cdf15dac05b12520acdad78
joey <joey_qpb@hotmail.com>
2024-01-18 10:44:58 +0100

    done

---
+++ src/tarefaM18/M18.java
@@ -0,0 +1,11 @@
+package tarefaM18;
+
+import annotations.Tabela;
+
+@Tabela(table_name = "Main_Table")
+public class M18 {
+	
+	public static void main(String[] args) {
+		System.out.println("done");
+	}
+}
